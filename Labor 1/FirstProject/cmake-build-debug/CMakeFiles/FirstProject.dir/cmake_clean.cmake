e3cb2a1159f08c795dac8391c76c8b295a8d395b
peti2255 <48029773+peti2255@users.noreply.github.com>
2021-02-18 14:10:46 +0200

    Első példa

---
+++ Labor 1/FirstProject/cmake-build-debug/CMakeFiles/FirstProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FirstProject.dir/functions.c.obj"
+  "CMakeFiles/FirstProject.dir/main.c.obj"
+  "FirstProject.exe"
+  "FirstProject.exe.manifest"
+  "FirstProject.pdb"
+  "libFirstProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FirstProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
