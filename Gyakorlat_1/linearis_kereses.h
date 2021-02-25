0f5e313bfbd648e8d3df1615af1ab8e2be680bd0
peti2255 <48029773+peti2255@users.noreply.github.com>
2021-02-25 21:42:36 +0200

    Delete Gyakorlat_1 directory

--- Gyakorlat_1/linearis_kereses.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by petee on 25/02/2021.
-//
-
-#ifndef GYAKORLAT_1_LINEARIS_KERESES_H
-#define GYAKORLAT_1_LINEARIS_KERESES_H
-int linearis_kereses(int*,int,int);
-//void kiir(int*,int);
-#endif GYAKORLAT_1_LINEARIS_KERESES_H
