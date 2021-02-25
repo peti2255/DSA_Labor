f3bac72bd6c914fdc41b35c686366b618b142b26
peti2255 <48029773+peti2255@users.noreply.github.com>
2021-02-25 13:40:36 +0200

    Labor gyakorlat

---
+++ Labor 1/FirstProject/functions.h
@@ -0,0 +1,37 @@
+//
+// Created by User on 2/18/2021.
+//
+
+#ifndef FIRSTPROJECT_FUNCTIONS_H
+#define FIRSTPROJECT_FUNCTIONS_H
+int osszeg(int, int);
+int max(int a1,int b1,int c1);
+int min(int a1,int b1,int c1);
+void feltolt(int*,int);
+void kiir(int*,int);
+int tosszeg(int*,int);
+int tszorzat(int*,int);
+int tatlag(int*,int);
+int osszeg(int a, int b);
+
+//Zsolt
+int szorzat(int num1, int num2);
+
+//Rebeka
+int osztasi_hanyados(int osztando, int oszto);
+
+//Robert
+double mertanikozep(double num1, double num2);
+
+//Zsofi
+double atlag(double num1, double num2, double num3);
+
+//Peter
+int osztasiMaradek(int osztando, int oszto);
+
+//Csongor
+double szamtaniKozep(double num1, double num2);
+
+//Szabolcs
+double harmonikusKozep(double num1, double num2, double num3);
+#endif //FIRSTPROJECT_FUNCTIONS_H
