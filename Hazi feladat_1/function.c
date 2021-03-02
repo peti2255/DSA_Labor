3247cd750d76743104011342637a4f6c10b48487
peti2255 <48029773+peti2255@users.noreply.github.com>
2021-03-02 20:57:53 +0200

    Hazi feladat 1

---
+++ Hazi feladat_1/function.c
@@ -0,0 +1,13 @@
+//
+// Created by petee on 02/03/2021.
+//
+#include "function.h"
+
+float idovizsgaloPerc(int a)
+{
+    return (float)a/60;
+}
+float idovizsgaloOra(int a)
+{
+    return (float)a/60;
+}
---
+++ Hazi feladat_1/function.h
@@ -0,0 +1,9 @@
+//
+// Created by petee on 02/03/2021.
+//
+
+#ifndef HAZI_FELADAT_1_FUNCTION_H
+#define HAZI_FELADAT_1_FUNCTION_H
+float idovizsgaloPerc(int);
+float idovizsgaloOra(int);
+#endif //HAZI_FELADAT_1_FUNCTION_H
